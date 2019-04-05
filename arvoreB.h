bfbc6fa9b72d958ddcd65ef5b011c22e6aa28f0c
groundon2 <49254188+groundon2@users.noreply.github.com>
2019-04-05 08:24:16 -0300

    Atualizei

--- arvoreB.h
+++ arvoreB.h
@@ -2,6 +2,7 @@
 #define ARVOREB_H
 
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -9,7 +10,8 @@ template <class TIPO>
 class arvoreB;
 
 template <class TIPO>
-class Nodo {
+class Nodo
+{
     friend class arvoreB<TIPO>;
 private:
     int quant_chaves;
@@ -25,24 +27,20 @@ public:
 template <class TIPO>
 Nodo<TIPO>::Nodo(int grau_max){
     this -> quant_chaves = 0;
-    //this -> apontadores = new Nodo<TIPO>[grau_max+1];
+    this -> apontadores = new Nodo<TIPO>[grau_max+1];
     this -> chaves = new TIPO[grau_max];
 }
 
 //destrutora da classe Nodo
 template <class TIPO>
-Nodo<TIPO>::~Nodo() {
+Nodo<TIPO>::~Nodo(){
     delete [] this -> chaves;
     delete [] this -> apontadores;
 }
 
 template <class TIPO>
-bool Nodo<TIPO>::eh_folha() {
-    return (this -> apontadores[0] == NULL);
-}
-
-template <class TIPO>
-class arvoreB {
+class arvoreB
+{
 private:
     int t, grau_max;
     Nodo<TIPO> *raiz = new Nodo<TIPO>(grau_max);
@@ -52,18 +50,38 @@ public:
     Nodo<TIPO>* Busca(Nodo<TIPO> *no,TIPO ch);
     void Insere(TIPO valor);
     void Insere(Nodo<TIPO> *ap,TIPO valor);
+    void Insere_No_Superlotado(Nodo<TIPO> *ap,TIPO valor,int i);
+    void Insere_No_Sublotado(Nodo<TIPO> *ap,TIPO valor,int i);
+    void Insere_Raiz_Nula(TIPO valor);
 };
 
 template <class TIPO>
-arvoreB<TIPO>::arvoreB(int t) {
+arvoreB<TIPO>::arvoreB(int t){
     this -> raiz = NULL;
     this -> t = t;
     this -> grau_max = 2 * t;//deixa 2t para poder inserir e depois partir no caso de superlotamento
 }
 
 template <class TIPO>
-arvoreB<TIPO>::~arvoreB() {
+arvoreB<TIPO>::~arvoreB(){
 
+}
+
+template <class TIPO>
+void arvoreB<TIPO>::Insere_Raiz_Nula(TIPO valor){
+    Nodo<TIPO>* novoNo = (Nodo<TIPO>*)malloc(sizeof(Nodo<TIPO>));
+
+    novoNo -> quant_chaves = 1;
+    novoNo -> chaves[0] = valor;
+    novoNo -> apontadores = NULL;
+    cout<<"Inseriu o valor "<<valor<<endl;
+    this->raiz = novoNo;
+}
+
+template <class TIPO>
+void arvoreB<TIPO>::Insere_No_Sublotado(Nodo<TIPO> *ap,TIPO valor,int i){
+
+    //Copiar os antigos apontadores daquele no
 }
 
 template <class TIPO>
@@ -73,41 +91,60 @@ void arvoreB<TIPO>::Insere(TIPO valor){
 }
 
 template <class TIPO>
-void arvoreB<TIPO>::Insere(Nodo<TIPO> *ap,TIPO valor) {
+void arvoreB<TIPO>::Insere(Nodo<TIPO> *ap,TIPO valor){
     int i;
-    Nodo<TIPO>* novoNo = (Nodo<TIPO>*)malloc(sizeof(Nodo<TIPO>));
-    novoNo -> chaves = (int*)malloc(sizeof(int)*4);
 
-    if(this -> raiz == NULL) {
-        novoNo -> quant_chaves = 1;
-        novoNo -> chaves[0] = valor;
-        novoNo -> apontadores = NULL;
-        cout<<"Inseriu o valor "<<valor<<endl;
-    }else{
-
+    if(ap == NULL){
+        Insere_Raiz_Nula(valor);
     }
-}
-
-/*
-Busca: Função para procurar em que nó está ou deve estar um determinado elemento
-Retorno: Um nó para ser feita a inserção ou que contém o número passado
-*/
-template <class TIPO>
-Nodo<TIPO>* arvoreB<TIPO>::Busca(Nodo<TIPO> *no,TIPO ch) {
-    int i = 0;
-    if(no == NULL){
-        cout << "Não tem esse registro na árvore!!!";
-        return no;
-    }else{
-        while(i < no->quant_chaves && ch > no->chaves[i])
-            i++;
-            if(ch == no->chaves[i])
-                return no;
-            if(ch < no->chaves[i])
-                Busca(no->apontadores[i-1],ch);
+    else{
+        int max_ch = 2 * t -1;
+        if(ap->quant_chaves + 1 == max_ch)  //Superlotado
+        {
+            for(i=0; i<ap->quant_chaves; i++)
+            {
+                if(valor < ap->chaves[i])
+                    break;
+                //Inserir valor no nó
+                //Insere_No_Superlotado(ap,valor,i);
+                //Quebrar e passar a mediana
+            }
+        }
             else
-                Busca(no->apontadores[i],ch);
+            {
+                for(i=0; i<ap->quant_chaves; i++)
+                {
+                    if(valor < ap->chaves[i])
+                        break;
+                }
+                //Inserir valor no nó
+                //Insere_No_Sublotado(ap,valor,i);
+            }
+        }
     }
-}
+
+    /*
+    Busca: Função para procurar em que nó está ou deve estar um determinado elemento
+    Retorno: Um nó para ser feita a inserção ou que contém o número passado
+    */
+    template <class TIPO>
+    Nodo<TIPO>* arvoreB<TIPO>::Busca(Nodo<TIPO> *no,TIPO ch)
+    {
+        int i;
+        if(no == NULL){
+            cout << "Não tem esse registro na árvore!!!";
+            return no->apontadores;
+        }
+        else{
+            for(i = 0; i < no->quant_chaves && ch > no->chaves[i]; i++)
+                i++;
+            if(ch == no->chaves[i])
+                return no->apontadores;
+            if(ch < no->chaves[i])
+                Busca(&no->apontadores[i-1],ch);
+            else
+                Busca(&no->apontadores[i],ch);
+        }
+    }
 
 #endif //ARVOREB_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <cstdio>
+#include <cstring>
 #include "arvoreB.h"
 
 using namespace std;
+
+void Leitura(arvoreB arvore){
+    int valor;
+    char linha[30], comando[20];
+    while(scanf("%[^\n]%s\n", linha)){
+        sscanf(linha, "%s %d", comando, valor);
+        if(strcmp(comando, "insere"))
+            arvore.Insere(valor);
+        else if(strcmp(comando, "busca"))
+            arvore.Busca(valor);
+        else if(strcmp(comando, "imprime_arvore")){
+
+        }
+        else
+            cout<<"Comando invalido"<<endl;
+    }
+
+}
 
 int main(){
     int t;
